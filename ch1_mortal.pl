99e33ae0f2e0922930cdadc72decc6c4990e7a5d
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2022-12-17 15:59:31 -0500

    Added ch1 code

---
+++ ch1_mortal.pl
@@ -0,0 +1,13 @@
+% MORTAL - the first illustrative prolog program
+
+mortal(X) :- person(X).
+
+person(socrates).
+person(plato).
+person(aristotle).
+
+mortal_report:-
+    write('Known mortals are:'),nl,
+    mortal(X),
+    write(X),nl,
+    fail.
